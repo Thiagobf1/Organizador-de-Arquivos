cf0d01d3e03d0b5ffe165c7286498a6c73afeb2d
Thiagobf1 <thiago.busifeijo@gmail.com>
2023-10-30 18:29:50 -0300

    End Project

--- Main.py
+++ Main.py
@@ -11,3 +11,11 @@ locais = {
   "pdfs": [".pdf"],
   "cvs": [".csv"]
 }
+
+for arquivo in lista_arquivos:
+  nome, extensao = os.path.splitext(f"{caminho}/{arquivo}")
+  for pasta in locais:
+    if extensao in locais[pasta]:
+      if not os.path.exists(f"{caminho}/{pasta}"):
+        os.makedirs(f"{caminho}/{pasta}")
+      os.rename(f"{caminho}/{arquivo}",f"{caminho}/{pasta}/{arquivo}")  
