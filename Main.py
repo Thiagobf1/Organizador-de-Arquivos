fb5b89122e53fbc14c4d9b6da01df04813ad9568
Thiagobf1 <thiago.busifeijo@gmail.com>
2023-10-30 18:20:43 -0300

    ADD Local File

--- Main.py
+++ Main.py
@@ -4,3 +4,10 @@ from tkinter.filedialog import askdirectory
 caminho = askdirectory(title="Selecione uma pasta")
 
 lista_arquivos = os.listdir(caminho)
+
+locais = {
+  "imagens": [".png", ",jpg"],
+  "planilhas": ["xlsx"],
+  "pdfs": [".pdf"],
+  "cvs": [".csv"]
+}
